f21e2cde910b02de02538ff656e4f953ce836757
Neandher Carlos da Silva <neandher.carlos@faesa.br>
2018-06-18 18:06:54 -0300

    ...

---
+++ src/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use App\Resource\Model\TimestampableTrait;
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    use TimestampableTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
