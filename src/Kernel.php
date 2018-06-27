13f46f8d1d66b86e5d301855ed0c19d01c50236b
Neandher Carlos da Silva <neandher.carlos@faesa.br>
2018-06-27 11:16:12 -0300

    ...

--- src/Kernel.php
+++ src/Kernel.php
@@ -15,6 +15,12 @@ class Kernel extends BaseKernel
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set( 'America/Sao_Paulo' );
+        parent::__construct($environment, $debug);
+    }
+
     public function getCacheDir()
     {
         return $this->getProjectDir().'/var/cache/'.$this->environment;
