9dc6f027c485ab58329aa2bda87bbaaf99083d08
Neandher Carlos <neandher89@gmail.com>
2020-07-02 01:33:54 -0300

    changes

---
+++ src/EventListener/OrderCreateSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Order;
+use App\Entity\Stock;
+use App\StockTypes;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class OrderCreateSubscriber implements EventSubscriber
+{
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::onFlush,
+        ];
+    }
+
+    public function onFlush(OnFlushEventArgs $args)
+    {
+        $em = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityInsertions() as $keyEntity => $entity) {
+            if ($entity instanceof Order) {
+                foreach ($entity->getOrderItems() as $orderItem) {
+                    $stockByReferency = $em->getRepository(Stock::class)->findOneBy(['referency' => $orderItem->getReferency()]);
+
+                    if (!$stockByReferency) {
+                        return new JsonResponse(['message' => 'Referência ' . $orderItem->getReferency() . ' não encontrada.'], 400);
+                    }
+
+                    $stock = new Stock();
+                    $stock
+                        ->setType(StockTypes::TYPE_REMOVE)
+                        ->setQuantity($orderItem->getQuantity())
+                        ->setAmount($stock->getQuantity() * $orderItem->getPrice())
+                        ->setUnitPrice($orderItem->getPrice())
+                        ->setCustomer(null)
+                        ->setPaymentMethod($entity->getPaymentMethod())
+                        ->setReferency($orderItem->getReferency())
+                        ->setBrand($stockByReferency->getBrand());
+
+                    $em->persist($stock);
+
+                    $classMetadata = $em->getClassMetadata(Stock::class);
+                    $uow->computeChangeSet($classMetadata, $stock);
+                }
+            }
+        }
+    }
+}
